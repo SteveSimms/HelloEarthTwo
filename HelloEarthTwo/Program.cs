9da9bec132d4faceaff7b11040ebaa7d0b1bd4f9
Steven Simms <simmsstev@gmail.com>
2021-08-27 15:30:05 -0700

    Refactored my code base into classes
    Refactored my app into a class with 2 methods.
    Next step break one of the methods out into its own class and call it from another class

--- HelloEarthTwo/Program.cs
+++ HelloEarthTwo/Program.cs
@@ -12,49 +12,62 @@ namespace HelloEarthTwo
     class Program
     {
         static void Main(string[] args)
-        {     
-      
-   
-            //Capture user input and write to a Json file
-            string heroName;
-            string powers;
-            string secretId;
-            string homeWorld;
-            Console.WriteLine($"Whats your code name? ");
-            heroName = Console.ReadLine();
-            Console.WriteLine($"Whats your powers? ");
-            powers = Console.ReadLine();
-            Console.WriteLine($"Whats your secret identity? ");
-            secretId = Console.ReadLine();
-            Console.WriteLine($"Whats your home world? ");
-            homeWorld = Console.ReadLine();
+        {
 
+            var heroFile = new Hero();
+            heroFile.CaptureUserInput(); 
 
-
-            Console.WriteLine($"{heroName}, {powers} , {secretId}, home world {homeWorld}");
-
-            Console.WriteLine($"{Environment.NewLine} Press any key to exit...");
-            Console.ReadKey(true);
-
-            string[] userInput = { heroName, powers, secretId, homeWorld };
-            // Abstract below code into a class and class method             // Create an object for array contents
-            var hero = new
-            {
-                codeName = heroName,
-                powers = powers,
-                secretId = secretId,
-                homeWorld = homeWorld
-
-            };
-
-            string output = JsonConvert.SerializeObject(hero); // converting hero Object to json  
-            Console.WriteLine(output);
-            // write json to .json file
-            File.WriteAllText(@"C:\Users\simms\learn\projects\HelloEarthTwo\HelloEarthTwo\hero.json", output); // Writing our hero information to hero.json file
 
             //Enhance the application to prompt the user for their name and display it along with the date and time.
             var CurrentDate = DateTime.Now;
             //Console.WriteLine($"{Environment.NewLine} Salam, {name} on {CurrentDate:d} at {CurrentDate:t}!");
+
+        }
+
+        public class Hero
+        {
+
+            //Method captures userInput and stores it into an Object 
+            public object CaptureUserInput()
+            {
+                Console.WriteLine($"Whats your code name? ");
+                var heroName = Console.ReadLine();
+                Console.WriteLine($"Whats your powers? ");
+                var powers = Console.ReadLine();
+                Console.WriteLine($"Whats your secret identity? ");
+                var secretId = Console.ReadLine();
+                Console.WriteLine($"Whats your home world? ");
+                var homeWorld = Console.ReadLine();
+
+                Console.WriteLine($"{heroName}, {powers} , {secretId}, home world {homeWorld}");
+
+                Console.WriteLine($"{Environment.NewLine} Press any key to exit...");
+                Console.ReadKey(true);
+
+                string[] userInput = { heroName, powers, secretId, homeWorld };
+
+                var hero = new
+                {
+                    codeName = heroName,
+                    powers = powers,
+                    secretId = secretId,
+                    homeWorld = homeWorld
+
+                };
+                ConvertHeroInput(hero);
+                return hero;
+            }
+
+            //Method Converts captured userInput to Json  and writes to hero.json file
+            public void ConvertHeroInput(object hero)
+            {
+                string output = JsonConvert.SerializeObject(hero); // converting hero Object to json  
+                Console.WriteLine(output);
+             
+                // write json to .json file
+                File.WriteAllText(@"C:\Users\simms\learn\projects\HelloEarthTwo\HelloEarthTwo\hero.json", output); // Writing our hero information to hero.json file
+               
+            }
 
 
         }
